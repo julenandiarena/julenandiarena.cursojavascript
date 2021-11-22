f142cf1dc9f4e8c7f695d2ae7130f0c196460e8b
julenandiarena <julenandiarena@gmail.com>
2021-11-22 12:48:34 -0300

    agregue la condicion para usuarios no registrados

--- clase02Andiarena/js/v1.1.0.js
+++ clase02Andiarena/js/v1.1.0.js
@@ -2,10 +2,13 @@
 Si su apellido y nombre son true se le habilita el acceso.
 */
 let message;
-let fullname = prompt("Por favor ingrese su nombre y apellido");
-if ((fullname == null) || (fullname == "")){
+let name = prompt("Por favor ingrese su nombre y apellido");
+if ((name == null) || (name == "")){
     message = "Has cancelado o introducido el nombre vacío. Por favor, intente nuevamente.";
-} else ((fullname == "Juan Perez") || (fullname == "juan perez"));{
-    message = "Hola Juan Perez bienvenido a la web de C.A.S.L.";
+}else if ((name === "Juan Perez") || (name === "juan perez")){
+    message = "Hola, Juan Perez, bienvenido a la web de C.A.S.L.";
+}else if ((name != "Juan Perez") || (name != "juan perez")){
+    message = "Usuario no registrado en la web de C.A.S.L. Por favor comuníquese a ingresos@casl.com.ar";
 }
 alert(message);
+console.log(name)
