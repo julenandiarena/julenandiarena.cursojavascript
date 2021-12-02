9fc110d7e0dcad917473391a892b6a3c843ccfb2
julenandiarena <julenandiarena@gmail.com>
2021-12-02 17:49:02 -0300

    Entrega v.1.5.0 y actualización del versionado

---
+++ clase05Andiarena/js/v1.5.0.js
@@ -0,0 +1,64 @@
+function profesional(){
+    let profesional = parseInt(prompt("Por favor ingrese su número de DNI"));
+    while(profesional != null && profesional != ""){
+        switch (profesional){
+            case 14393827:
+                alert("Bienvenido a la web de CASL: Lic. Juan Pérez. Pulse 'Aceptar' para continuar.");
+                break
+            case 13764831:
+                alert("Bienvenida a la web de CASL: Lic. María López. Pulse 'Aceptar' para continuar.");
+                break
+            default:
+                alert("DNI no ingresado o inválido. Acceso denegado, por favor comunicarse a registros@casl.com.ar para completar el registro.");
+                continue
+            }
+            break
+        }
+    }
+
+profesional();
+
+function Paciente(name, surname, age, passport, adress, meds, medsnumber, diagnosis, clinichistory) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.passport = passport;
+     this.adress = adress;
+    this.meds = meds;
+    this.medsnumber = medsnumber;
+    this.diagnosis = diagnosis;
+    this.clinichistory = clinichistory;
+    }
+    
+const name = prompt("Ingrese NOMBRE del paciente:");
+const surname = prompt("Ingrese APELLIDO del paciente:");
+const age = parseInt(prompt("Ingrese EDAD del paciente:"));
+const passport = parseInt(prompt("Ingrese NÚMERO DE DNI del paciente:"));
+const adress = prompt("Ingrese DOMICILIO del paciente:");
+const meds = prompt("Ingrese OBRA SOCIAL del paciente:");
+const medsnumber = prompt("Ingrese NÚMERO DE OBRA SOCIAL del paciente:");
+const diagnosis = prompt("Ingrese DIAGNÓSTICO del paciente:");
+const clinichistory = prompt("Ingrese HISTORIA CLÍNICA del paciente:");
+
+let paciente1 = new Paciente (name, surname, age, passport, adress, meds, medsnumber, diagnosis, clinichistory);
+
+console.log("name" in paciente1);
+console.log("surname" in paciente1);
+console.log("age" in paciente1);
+console.log("passport" in paciente1);
+console.log("adress" in paciente1);
+console.log("meds" in paciente1);
+console.log("medsnumber" in paciente1);
+console.log("diagnosis" in paciente1);
+console.log("clinichistory" in paciente1);
+console.log(paciente1);
+
+document.getElementById("name").innerHTML = paciente1.name;
+document.getElementById("surname").innerHTML = paciente1.surname;
+document.getElementById("age").innerHTML =  paciente1.age;
+document.getElementById("passport").innerHTML =  paciente1.passport;
+document.getElementById("adress").innerHTML = paciente1.adress;
+document.getElementById("meds").innerHTML = paciente1.meds;
+document.getElementById("medsnumber").innerHTML =  paciente1.medsnumber;
+document.getElementById("diagnosis").innerHTML =  paciente1.diagnosis;
+document.getElementById("clinichistory").innerHTML =  paciente1.clinichistory;
