d7d314489fa11fd2d257c255a9aba8cd79587265
julenandiarena <julenandiarena@gmail.com>
2021-12-03 17:15:52 -0300

    corregi la falta de camelCase

--- clase05Andiarena/js/v1.5.0.js
+++ clase05Andiarena/js/v1.5.0.js
@@ -18,16 +18,16 @@ function profesional(){
 
 profesional();
 
-function Paciente(name, surname, age, passport, adress, meds, medsnumber, diagnosis, clinichistory) {
+function Paciente(name, surname, age, passport, adress, meds, medsNumber, diagnosis, clinicHistory) {
     this.name = name;
     this.surname = surname;
     this.age = age;
     this.passport = passport;
      this.adress = adress;
     this.meds = meds;
-    this.medsnumber = medsnumber;
+    this.medsNumber = medsNumber;
     this.diagnosis = diagnosis;
-    this.clinichistory = clinichistory;
+    this.clinicHistory = clinicHistory;
     }
     
 const name = prompt("Ingrese NOMBRE del paciente:");
@@ -36,11 +36,11 @@ const age = parseInt(prompt("Ingrese EDAD del paciente:"));
 const passport = parseInt(prompt("Ingrese NÚMERO DE DNI del paciente:"));
 const adress = prompt("Ingrese DOMICILIO del paciente:");
 const meds = prompt("Ingrese OBRA SOCIAL del paciente:");
-const medsnumber = prompt("Ingrese NÚMERO DE OBRA SOCIAL del paciente:");
+const medsNumber = prompt("Ingrese NÚMERO DE OBRA SOCIAL del paciente:");
 const diagnosis = prompt("Ingrese DIAGNÓSTICO del paciente:");
-const clinichistory = prompt("Ingrese HISTORIA CLÍNICA del paciente:");
+const clinicHistory = prompt("Ingrese HISTORIA CLÍNICA del paciente:");
 
-let paciente1 = new Paciente (name, surname, age, passport, adress, meds, medsnumber, diagnosis, clinichistory);
+let paciente1 = new Paciente (name, surname, age, passport, adress, meds, medsNumber, diagnosis, clinicHistory);
 
 console.log("name" in paciente1);
 console.log("surname" in paciente1);
@@ -48,9 +48,9 @@ console.log("age" in paciente1);
 console.log("passport" in paciente1);
 console.log("adress" in paciente1);
 console.log("meds" in paciente1);
-console.log("medsnumber" in paciente1);
+console.log("medsNumber" in paciente1);
 console.log("diagnosis" in paciente1);
-console.log("clinichistory" in paciente1);
+console.log("clinicHistory" in paciente1);
 console.log(paciente1);
 
 document.getElementById("name").innerHTML = paciente1.name;
@@ -59,6 +59,6 @@ document.getElementById("age").innerHTML =  paciente1.age;
 document.getElementById("passport").innerHTML =  paciente1.passport;
 document.getElementById("adress").innerHTML = paciente1.adress;
 document.getElementById("meds").innerHTML = paciente1.meds;
-document.getElementById("medsnumber").innerHTML =  paciente1.medsnumber;
+document.getElementById("medsNumber").innerHTML =  paciente1.medsNumber;
 document.getElementById("diagnosis").innerHTML =  paciente1.diagnosis;
-document.getElementById("clinichistory").innerHTML =  paciente1.clinichistory;
+document.getElementById("clinicHistory").innerHTML =  paciente1.clinicHistory;
