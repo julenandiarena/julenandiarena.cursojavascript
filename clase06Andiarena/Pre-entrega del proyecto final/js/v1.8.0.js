47e3f62ed2b6040af993171cbb8da565b9ec76c2
julenandiarena <julenandiarena@gmail.com>
2021-12-05 16:55:08 -0300

    borrador de la primera entrega del proyecto final

--- clase06Andiarena/Pre-entrega del proyecto final/js/v1.8.0.js
+++ clase06Andiarena/Pre-entrega del proyecto final/js/v1.8.0.js
@@ -1,22 +1,35 @@
-function profesional(){
-    let profesional = parseInt(prompt("Por favor ingrese su número de DNI:"));
-    while(profesional != null && profesional != ""){
-        switch (profesional){
-            case 14393827:
-                alert("Bienvenido a la web de CASL: Lic. Juan Pérez. Pulse 'Aceptar' para continuar.");
-                break
-            case 13764831:
-                alert("Bienvenida a la web de CASL: Lic. María López. Pulse 'Aceptar' para continuar.");
-                break
-            default:
-                alert("DNI no ingresado o inválido. Acceso denegado, por favor comunicarse a registros@casl.com.ar para completar el registro.");
-                continue
-            }
-            break
-        }
-    }
+//COSAS A AGREGAR O CORREGIR PARA LA PRIMERA ENTREGA DEL PROYECTO FINAL
+//Función profesional, hacerla array de objeto donde se guarden los admitidos.
+//*) entradaNombre === "esc" || entradaNombre === "ESC" || entradaNombre === "Esc"... resolver usando el metodo de string, toLowerCase().
+//ordenadosEdad = arrayPacientes; aca podrias haber usado spread operetor:[...arraypacientes].
+//Que ordene les pacientes por apellido en orden alfábetico, no por edad.
 
-profesional();
+const registeredProfessionals = [
+    {name: "Juan Pérez", numberID: "12345678"}, 
+    {name: "María López", numberID: "87654321"},
+    {name: "Susana Suárez", numberID: "11111111"}
+]
+function entryProfessionals(){
+    let entryProfessionals = prompt("Por favor ingrese su número de DNI");
+while(entryProfessionals != null && entryProfessionals != ""){
+    switch (registeredProfessionals){
+        case "12345678":
+            alert(`Bienvenido a la web de CASL: Lic. ${name}. Pulse 'Aceptar' para continuar.`);
+            break
+        case "87654321":
+            alert("Bienvenida a la web de CASL: Lic. María López. Pulse 'Aceptar' para continuar.");
+               break
+        case "11111111":
+            alert("Bienvenida a la web de CASL: Lic. Susana Suárez. Pulse 'Aceptar' para continuar.");
+            break
+        default:
+            alert("DNI no ingresado o inválido. Acceso denegado, por favor comunicarse a registros@casl.com.ar para completar el registro.");
+            continue
+        }
+        break
+    }
+}
+
 
 class Paciente{
     constructor(passport, name, surname, age, adress, meds, medsNumber, diagnosis, clinicHistory) {
@@ -34,26 +47,26 @@ class Paciente{
 
 const arrayPacientes = [];
 do{
-    var entradaDNI = prompt("Ingrese el número de DNI del paciente para nuevo registro o pulse 'esc' para terminar.");
-    if (entradaDNI === "esc" || entradaDNI === "ESC" || entradaDNI === "Esc"){
+    let entradaDNI = prompt("Ingrese el número de DNI del paciente para nuevo registro o pulse 'esc' para terminar.");
+    if (entradaDNI.toLowerCase() === "esc"){
         break;   
     }else{
         dniP = entradaDNI;
-        var nombreP = prompt("Ingrese nombre del paciente:");
-        var apellidoP = prompt("Ingrese apellido del paciente:");
-        var edadP = prompt("Ingrese edad del paciente:");
-        var direccionP= prompt("Ingrese dirección del paciente:");
-        var obraSocialP  = prompt("Ingrese obra social del paciente:");
-        var obraSocialNumeroP = prompt("Ingrese n° de afiliadx del paciente:");
-        var diagnosticoP= prompt("Ingrese sigla o código del diagnóstico del paciente:");
-        var historiaClinicaP = prompt("Ingrese historia clínica del paciente:");
-        arrayPacientes.push(new Paciente(dniP, nombreP, apellidoP, edadP, direccionP, obraSocialP, obraSocialNumeroP, diagnosticoP, historiaClinicaP));
+        let nombrePac = prompt("Ingrese nombre del paciente:");
+        let apellidoPac = prompt("Ingrese apellido del paciente:");
+        let edadPac = prompt("Ingrese edad del paciente:");
+        let direccionPac= prompt("Ingrese dirección del paciente:");
+        let obraSocialPac  = prompt("Ingrese obra social del paciente:");
+        let obraSocialNumeroPac = prompt("Ingrese n° de afiliadx del paciente:");
+        let diagnosticoPac= prompt("Ingrese sigla o código del diagnóstico del paciente:");
+        let historiaClinicaPac = prompt("Ingrese historia clínica del paciente:");
+        arrayPacientes.push(new Paciente(dniPac, nombrePac, apellidoPac, edadPac, direccionPac, obraSocialPac, obraSocialNumeroPac, diagnosticoPac, historiaClinicaPac));
     }
 } 
-while (entradaDNI != "esc" || entradaDNI != "ESC" || entradaDNI != "Esc")
+while (entradaDNI != "esc")
 console.log(arrayPacientes);
 
-for (var paciente of arrayPacientes){
+for (let paciente of arrayPacientes){
     document.write("<ul><li><h3>DNI: " + paciente.passport + "</h3></li>");
     document.write("<ul><li><h3>Nombre: " + paciente.name + "</h3></li>");
     document.write("<ul><li><h3>Apellido: " + paciente.surname + "</h3></li>");
@@ -73,3 +86,12 @@ console.log(paciente.meds);
 console.log(paciente.medsNumber);
 console.log(paciente.diagnosis);
 console.log(paciente.clinicHistory);
+
+let ordenarApellidos = [];
+ordenarApellidos = arrayPacientes.map(surname => surname);
+ordenarApellidos = arrayPacientes;
+ordenarApellidos.sort(function(a, b){return (a.type < b.type) ? -1 : 1})
+console.log("Ordenados por Apellido en orden alfabético:");
+console.log(ordenarApellidos);
+document.write("<h3> Lista de Pacientes ordenados alfabéticamente por apellido: </h3>");
+
